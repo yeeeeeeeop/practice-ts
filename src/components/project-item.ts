e287f3c5ed629ba88f0ee23b3213aa1df0c16fcb
yeeeeeeeop <yeop0916@gmail.com>
2024-03-11 22:17:23 +0900

    feat: 웹팩 적용

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Draggable } from '../models/drag-drop.js';
-import Component from './base-component.js';
-import { Project } from '../models/project.js';
-import { AutoBind } from '../decorators/autobind.js';
+import { Draggable } from '../models/drag-drop';
+import Component from './base-component';
+import { Project } from '../models/project';
+import { AutoBind } from '../decorators/autobind';
 
 // ProjectItem Class
 export default class ProjectItem
---
+++ webpack.config.prod.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // devtool: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+};
